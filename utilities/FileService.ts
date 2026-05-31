11497d3f10dbb2f43a455f659d377c38346e9a13
searsbd <searsbd@miamioh.edu>
2026-05-30 22:19:55 -0400

    added direct json support

--- utilities/FileService.ts
+++ utilities/FileService.ts
@@ -15,7 +15,7 @@ export class FileService {
      * @param isCompressed whether or not the file that you are trying to read is compressed
      * @returns the plaintext contents of the file that you specified
      */
-    public static readFile(fileName: string, isCompressed: boolean = false): string {
+    public static readFileString(fileName: string, isCompressed: boolean = false): string {
         if (!isCompressed) {
             const plaintextData: string = fs.readFileSync(fileName, 'utf-8');
             return plaintextData;
@@ -28,19 +28,44 @@ export class FileService {
     }
 
     /**
+     * Reads a file and returns the json contained with in, works for compressed
+     * and uncompressed files
+     * 
+     * @param fileName The name of the file you want to read
+     * @param isCompressed Whether or not that file is compressed
+     * @returns The JSON contained in that file
+     */
+    public static readFileJSON(fileName: string, isCompressed: boolean = false): JSON {
+        const plaintextContents: string = FileService.readFileString(fileName, isCompressed);
+        return JSON.parse(plaintextContents);
+    }
+
+    /**
      * Writes the specified contents to the given file name, with the option to
      * compress it.
      * 
      * @param fileName the name of the file you want to write to
      * @param compressFile whether or not you should compress the file
      */
-    public static writeFile(fileName: string, fileContents: string, compressFile: boolean = false): void {
+    public static writeFileString(fileName: string, fileContents: string, compressFile: boolean = false): void {
         if (!compressFile) {
             fs.writeFileSync(fileName, fileContents, 'utf-8')
         } else {
             const compressedFileContents: Buffer = gzipSync(fileContents)
             fs.writeFileSync(fileName, compressedFileContents);
         }
+    }
+
+    /**
+     * Takes in JSON and a file name and writes the file, can compress file.
+     * 
+     * @param fileName The name of the file that you want to write to
+     * @param fileContents the json information of the file you want to write
+     * @param compressFile Whether or not you want to compress the file.
+     */
+    public static writeFileJSON(fileName: string, fileContents: JSON, compressFile: boolean = false): void {
+        const stringFileContents: string = JSON.stringify(fileContents);
+        FileService.writeFileString(fileName, stringFileContents, compressFile);
     }
 
 }
